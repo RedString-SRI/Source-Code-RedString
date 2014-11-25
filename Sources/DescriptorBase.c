342f014230d97fff38056680ddd92e55494b9bee
gabond <gab-dr@hotmail.fr>
2014-11-25 21:30:12 +0100

    Update DescriptorBase.c

--- Sources/DescriptorBase.c
+++ Sources/DescriptorBase.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include<string.h>
+
 void clearBuffer() {
 int c = 0;
 while (c != '\n' && c != EOF)
@@ -6,4 +9,18 @@ c = getchar();
 
 void initStack(DescriptorBase *p) {
   
+}
+
+
+BOOL pathExist(char[] path) {
+  int answer;
+  char sentence[50]="[ ! -f ";
+  char sentenceEnd[5]=" ]";
+  strcat(sentence,path);
+  strcat(sentence,sentenceEnd);
+  answer=system(sentence);
+  if(answer==0)
+    return FALSE;
+  else
+    return TRUE;
 }

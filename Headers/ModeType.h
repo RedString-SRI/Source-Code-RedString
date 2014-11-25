477171b4d4422b22e93ae55a796990a61173dd69
chabaudmorgan <chabaudmorgan@gmail.com>
2014-11-25 16:01:20 -0600

    Updates for doxygen

--- Headers/ModeType.h
+++ Headers/ModeType.h
@@ -1,6 +1,22 @@
-#ifndef MODE_TYPE
-#define MODE_TYPE
+/**
+ * \file Bool.h
+ * \brief Define a writing mode type for descriptors.
+ * \author Morgan Chabaud
+ * \date 25 november 2014
+ * 
+ *
+ */
 
+#ifndef MODE_T
+#define MODE_T
+
+/**
+ * \enum Mode
+ * \brief It is a writing mode type for descriptors.
+ * 
+ * This type is used to know if the descriptor
+ * could be automatically indexed or not
+ */
 typedef enum {AUTO, MANUAL} Mode;
 
 #endif

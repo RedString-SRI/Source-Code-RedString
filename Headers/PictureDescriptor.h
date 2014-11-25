11b81295a1476ead1108d1d7e55737e9ea2127e7
gabond <gab-dr@hotmail.fr>
2014-11-25 20:40:08 +0100

    Update PictureDescriptor.h

--- Headers/PictureDescriptor.h
+++ Headers/PictureDescriptor.h
@@ -12,4 +12,32 @@ typedef struct{
   
 }PictureDescriptor;
 
+/*Initialize a new picture's descriptor*/
+void initPictureDescriptor(PictureDescriptor *pd);
+
+/*Change mode*/
+void setMode(Mode *m);
+
+/*return mode*/
+Mode getMode();
+
+/**/
+void setNbcomp(int n);
+
+/*return nbcomp*/
+int getNbcomp();
+
+/**/
+void setNbquantif(int n);
+
+/*return nbquantif*/
+int getNbquantif();
+
+/*Creat the image's histogramm*/
+void createHistogramm();
+
+/*Print the image's histogramm*/
+void printHistogramm();
+
+
 #endif

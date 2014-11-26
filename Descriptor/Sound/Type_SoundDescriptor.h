a93234a19773fde5f6466abac1e81ad910479ed5
chabaudmorgan <chabaud.morgan@gmail.com>
2014-11-26 16:53:24 -0600

    Updated architecture

---
+++ Descriptor/Sound/Type_SoundDescriptor.h
@@ -0,0 +1,27 @@
+/**
+ * \file Type_SoundDescriptor.h
+ * \brief Sound Descriptor Type
+ * \author Morgan Chabaud
+ */
+
+#include "Type_WritingMode.h"
+
+#ifndef TYPE_SOUND_DESCRIPTOR
+#define TYPE_SOUND_DESCRIPTOR
+
+/**
+ * \struct SoundDescriptor
+ * \brief A sound descriptor
+ *
+ * A sound descriptor is a descriptor. It is used to define
+ * a sound file. This is kind of a summary of it with some losses.
+ */
+typedef struct{
+  int id;       /*!< Idendifier of the descriptor. It is unique. */
+  Mode writingMode;     /*!< This is the writing mode. */
+  double** histogram;   /*!< This histogram sizes k*m.
+                            k : Windows number
+                            m : Intervals number */
+}SoundDescriptor;
+
+#endif
---
+++ Descriptor/Text/TextDescriptor.h
@@ -0,0 +1,14 @@
+/**
+ * \file TextDescriptor.h
+ * \brief
+ * \author
+ * \date 
+ *
+ */
+
+#include "Type_TextDescriptor.h"
+
+#ifndef TEXTDESCRIPTOR
+#define TEXTDESCRIPTOR
+
+#endif

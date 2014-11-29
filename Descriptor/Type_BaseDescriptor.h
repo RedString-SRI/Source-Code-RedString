5786e55c37622628ac73049b88ab4f8100c28fea
Rayope <chabaud.morgan@gmail.com>
2014-11-29 16:21:57 +0100

    Update Type_BaseDescriptor.h

--- Descriptor/Type_BaseDescriptor.h
+++ Descriptor/Type_BaseDescriptor.h
@@ -1,5 +1,10 @@
 
+#ifndef TYPE_BASE_DESCRIPTOR
+#define TYPE_BASE_DESCRIPTOR
+
 typedef struct Descriptor{
   void * element;
   struct Descriptor * next;
 } *BaseDescriptor; // DescriptorBase is represented by a stack of generic Descriptors, and is a pointer to the first element of the stack
+
+#endif

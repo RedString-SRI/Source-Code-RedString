b5faa250794011db1fd65c02690970d89dd454c5
gabond <gab-dr@hotmail.fr>
2014-11-29 22:32:17 +0100

    Update PictureDescriptor.c

--- Descriptor/Picture/PictureDescriptor.c
+++ Descriptor/Picture/PictureDescriptor.c
@@ -1,7 +1,7 @@
 #include "PictureDescriptor.h
 "
 /*Initialize a new picture's descriptor*/
-PictureDescriptor PictureDescriptor(FILE* file) {
+PictureDescriptor initPictureDescriptor(FILE* file) {
    int pd->id=;
   Mode pd->writingMode=AUTO;
   int pd->nbcomp = 0;
@@ -15,6 +15,42 @@ Dimension getSizePicture(FILE *fileIMG) {
    fscanf(fileIMG,"%d", dim->width); 
    fscanf(fileIMG,"%d", dim->height);
    return dim;
+}
+
+PictureDescriptor createPictureDescriptor(FILE *fileIMG){
+   Dimension size;
+   int matrixRED , *matrixGREEN , *matrixBLUE;
+   int pixelIntensityRED[256] , int pixelIntensityGREEN[256] , int pixelIntensityBLUE[256];
+   int i , j;
+   
+   fopen(fileIMG);
+   size=getSizePicture(fileIMG);
+   
+   matrixRED=(*int)malloc(size.width*sizeof(int)); //matrix 1 dimension ...
+   for(i=0 ; i<size.heigth ; i++)
+   *matrixRED[i]=(*int)malloc(sizeof(int)); // grow up into matrix with 2 dimensions
+   
+         matrixGREEN=(*int)malloc(size.width*sizeof(int)); 
+         for(i=0 ; i<size.height ; i++)
+         *matrixGREEN[i]=(*int)malloc(sizeof(int)); 
+         
+   matrixBLUE=(*int)malloc(size.width*sizeof(int)); 
+   for(i=0 ; i<size.height ; i++)
+   *matrixBLUE[i]=(*int)malloc(sizeof(int));
+      
+   for(i=0 ; i<size.wdith ; i++){ // scan the whole matrix : 1rst Red
+      for(j=0; j<size.height ; j++)
+         fscanf(fileIMG , "%d" , matrixRED[i][j]);
+   }
+   for(i=0 ; i<size.wdith ; i++){ // scan the whole matrix : 2nd green
+      for(j=0; j<size.height ; j++)
+         fscanf(fileIMG , "%d" , matrixGREEN[i][j]);
+   }
+   for(i=0 ; i<size.wdith ; i++){ // scan the whole matrix : 3th blue
+      for(j=0; j<size.height ; j++)
+         fscanf(fileIMG , "%d" , matrixBLUE[i][j]);
+   }
+   fclose(fileIMG);
 }
 
 /*Change mode*/

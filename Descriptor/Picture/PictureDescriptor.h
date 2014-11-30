6dd6c0d5a3b90d1ae520a42f15b425f6aeb9357d
gabond <gab-dr@hotmail.fr>
2014-11-30 16:11:30 +0100

    Update PictureDescriptor.h

--- Descriptor/Picture/PictureDescriptor.h
+++ Descriptor/Picture/PictureDescriptor.h
@@ -1,4 +1,11 @@
-
+/**
+ * \file PictureDescriptor.h
+ * \brief picture descriptor
+ * \author Gabriel.DR
+ * \version 0.1
+ * \date 30 November 2014
+ *
+ */
 
 #include "Type_PictureDescritor.h"
 
@@ -9,30 +16,14 @@
 void initPictureDescriptor(PictureDescriptor *pd);
 
 /**
- * Take the first line of the text file about the linked image.
+ *\brief Give the size of the picture 
  */
-void getSizePicture(FILE *fileIMG, PictureDescriptor *descriptIMG);
+Dimension getSizePicture(FILE *fileIMG);
 
 /**
- * Take the Nsize next lines which are the linked red matrix of the image...
+ *\brief create the picture's descriptor
  */
-void getMatrixRED(FILE *fileIMG, PictureDescriptor *descriptIMG);
-
-/**
- * ... the next Nsize lines, the linked green matric ...
- */
-void getMatrixGREEN(FILE *fileIMG, PictureDescriptor *descriptIMG);
-
-/**
- * and the next Nsize lines, the matrix of blue of the linked image.
- */
-void getMatrixBLUE(FILE *fileIMG, PictureDescriptor *descriptIMG);
-
-/*Change mode*/
-void setMode(Mode *m);
-
-/*return mode*/
-Mode getMode();
+PictureDescriptor createPictureDescriptor(FILE *fileIMG);
 
 /**/
 void setNbcomp(int n);
@@ -40,17 +31,25 @@ void setNbcomp(int n);
 /*return nbcomp*/
 int getNbcomp();
 
-/**/
+/**
+ *\brief Permite to know how many high bits functions need to read
+*/
 void setNbquantif(int n);
 
-/*return nbquantif*/
+/**
+*\brief return nbquantif
+*/
 int getNbquantif();
 
-/*Creat the image's histogramm*/
-void createHistogramm();
+/**
+ *\brief Creat the image's histogramm
+*/
+void createHistogram(int bit[] , Dimension dim , int quantif);
 
-/*Print the image's histogramm*/
-void printHistogramm();
+/**
+ *\brief Print the image's histogramm
+*/
+void printHistogram(FILE *descriptIMG);
 
 
 #endif

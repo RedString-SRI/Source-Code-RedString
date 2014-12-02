5ad06ac73849d8dd3930133d8ab0f2e1c212d856
chabaudmorgan <chabaud.morgan@gmail.com>
2014-12-02 16:06:00 -0600

    Some keyboard input secured functions

---
+++ Keyboard.c
@@ -0,0 +1,17 @@
+/**
+ * \file Keyboard.h
+ * \brief Secured functions to interact with user
+ * \author Morgan Chabaud \n
+ */
+
+int getKeyboard_Long(long * typedLong, long min, long max)
+{
+}
+
+int getKeyboard_Double(double * typedDouble, double min, double max)
+{
+}
+
+int getKeyboard_String(char * typedString, int minChar, int maxChar)
+{
+}

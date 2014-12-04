186daac5813a8828bf9e5742a8171e771e15d49f
gabond <gab-dr@hotmail.fr>
2014-12-04 19:59:34 +0100

    Update Globals.c

--- Globals.c
+++ Globals.c
@@ -6,16 +6,16 @@
  * \author Morgan Chabaud \n
  * How to use : http://c.developpez.com/faq/?page=Les-variables-globales
  */
- 
+//=================================================================================================== 
 // TextDescriptor
 int globs_occurThreshold; // The minimum occurrences (in the indexed file) to store a word in a descriptor 
 int globs_maxTerms; // The number of word to keep in the descriptor
-
+//===================================================================================================
 // SoundDescriptor
 int globs_windowSize; // The sound signal will be split in x windows. So it is their size.
 int globs_nbElemInterval; // There is intervals in a window.
                           // They contains that number of elements (value of of a little time during signal).
-                          
+//===================================================================================================
 // PictureDescriptor
 int globs_nbWeightyBits; // The number of weighty bits to get on each pixel component
 int globs_compTolerance;  // The comparison tolerance for pixels

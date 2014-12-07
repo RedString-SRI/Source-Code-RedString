b3e6dcec13842f1cc7676bbb68c00c02d6b1eb5b
gabond <gab-dr@hotmail.fr>
2014-12-07 10:39:41 +0100

    Update SearchEngine.h

--- SearchEngine/SearchEngine.h
+++ SearchEngine/SearchEngine.h
@@ -16,7 +16,54 @@
 /**
 * \brief byColor.
 * Search specifiquely in an image's descriptor a color:
-*
+* BLACK:    00 00 00 -> 0
+* LIGHTGRAY:10 10 10 -> 42
+* DARKGRAY: 01 01 01 -> 21
+* WHITE:    11 11 11 -> 63
+* RED:      11 00 00 -> 48
+* PINK:     11 10 10 -> 58
+* YELLOW:   00 11 11 -> 15
+* PURPLE:
+* BROWN:
+* GREEN:    00 11 00 -> 12
+* BLUE:     00 00 11 -> 3
+* 
 *
 */
 void byColor(char color[]);
+
+/**
+* \brief byAuthor.
+* Search in whichever descriptor a author in alphabetic order.
+*/
+void byAuthor();
+
+/**
+* \brief byModifDate.
+* Search specifiquely in whichever descriptor the recent modification in increasing order.
+*/
+void byModifDate();
+
+/**
+* \brief byCreatingDate.
+* Search specifiquely in whichever descriptor by Creating Date in increasing order.
+*/
+void byCreatingDate();
+
+/**
+* \brief byNameFile.
+* Search specifiquely in whichever descriptor by Name's file in alphabetic order.
+*/
+void byNameFile();
+
+/**
+* \brief byOccurenceWord.
+* Search specifiquely in texte descriptor the files which get the most of researched words.
+*/
+void byOccurenceWord();
+
+/**
+* \brief bySOUNDSOUNDSOUNDSOUND.
+* 
+*/
+void bySOUNDSOUNDSOUND(); // I DONT KNOW WHAT IT IS RESEARCHED IN SOUND FILES !!!!!

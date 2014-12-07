58dce3401da027fdea9dc0fac515c15b194277e4
gabond <gab-dr@hotmail.fr>
2014-12-07 10:14:37 +0100

    Update SearchEngine.h

---
+++ SearchEngine/SearchEngine.h
@@ -0,0 +1,22 @@
+/**
+* \file SearchEngine.h
+* \brief listing of research functions.
+* \author Gabriel DEL REY
+* \version 0.1
+* \date 7 December 2014
+*
+* All the way to do research in the listeBaseDescriptor
+*
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+* \brief byColor.
+* Search specifiquely in an image's descriptor a color:
+*
+*
+*/
+void byColor(char color[]);

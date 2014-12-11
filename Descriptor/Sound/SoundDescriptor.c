425cd48dfe388f311c2eaea8e5e4f5119fc85fb1
gabond <gab-dr@hotmail.fr>
2014-12-11 10:58:30 +0100

    Create SoundDescriptor.c

---
+++ Descriptor/Sound/SoundDescriptor.c
@@ -0,0 +1,19 @@
+
+/**
+* \file SoundDescriptor.c
+* \brief Detailed functions for Sound file.
+* \author Gabriel DEL REY
+* \version 0.1
+* \date 6 December 2014
+*
+* Detailed functions for Sound file.
+*
+*/
+
+#include<SoundDescriptor.h>
+#include<Type_SoundDescriptor.h>
+
+void main() {
+  
+  
+}

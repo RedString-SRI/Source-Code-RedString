176fab7ed223a4ef45aa9572bbafbb49f3110a60
gabond <gab-dr@hotmail.fr>
2014-12-29 16:30:30 +0100

    Update interface.c

--- Interface/interface.c
+++ Interface/interface.c
@@ -7,7 +7,6 @@
 
 void IndexationMenu();
 void ResearchMenu();
-void messageError(int i);
 void mainMenu();
 void clearBuffer();
 //========================================================
@@ -15,19 +14,6 @@ void clearBuffer() {
     int c = 0;
     while (c != '\n' && c != EOF)
         c = getchar();
-}
-//========================================================
-void messageError(int i) {
-	switch(i) {
-	case(0): printf("ERROR : error411\n\n"); break;
-	case(1): printf("ERROR : Wrong choice\n\n"); break;
-	case(2): printf("ERROR : Unexisting path\n\n"); break;
-	case(3): printf("ERROR : Wrong option\n\n"); break;
-	case(4): printf("ERROR : \n\n"); break;
-	case(5): printf("ERROR : \n\n"); break;
-	case(6): printf("ERROR : \n\n"); break;
-	case(7): printf("ERROR : \n\n"); break;
-	}
 }
 //========================================================
 void mainMenu(){
@@ -44,7 +30,6 @@ void mainMenu(){
 		scanf("%d" , &choice);
 		if(choice!=1 && choice !=2 && choice!=0){
 			system("clear");
-			messageError(1);
 		}
 	}while(choice!=1 && choice!=2 && choice!=0);
 	clearBuffer();
@@ -81,13 +66,17 @@ void IndexationMenu(){
 		scanf("%d" , &choice);
 		if(choice!=1 && choice !=2 && choice!=0){
 			system("clear");
-			messageError(1);
 		}
 	}while(choice!=1 && choice!=2 && choice!=0);
 	clearBuffer();
 	switch(choice){
 		case(1):
-			system("clear");  
+			printf("Enter your path : \n");
+			scanf("%s" , w); // enter a path ......
+			if(ExistingPath(w)){
+				system("clear");
+			}
+			else printf("ERROR Path\n");  
 			break;
 		case(2):  
 			system("clear");
@@ -97,7 +86,6 @@ void IndexationMenu(){
 			mainMenu(0); 
 			break;
 		default: 
-			messageError(0) ; 
 			exit(0); 
 			break;
 	}
@@ -105,6 +93,8 @@ void IndexationMenu(){
 //========================================================
 void ResearchMenu(){
 	char a;
+	char w[];
+	char freq;
 	char comm[6]="ls -";
 	// FOR TESTING COMMAND UNIX
 	int choice;
@@ -115,39 +105,48 @@ void ResearchMenu(){
 		printf("|1. Research a word       |\n");
 		printf("|2. Research a color      |\n");
 		printf("|3. Research a sound      |\n");
-		printf("|4. Test command UNIX     |\n");
 		printf("|                         |\n");
 		printf("+=================0.RETURN+\n");
 
 		scanf("%d" , &choice);
-		if(choice!=1 && choice !=2 && choice!=0 && choice!=4){
+		if(choice!=1 && choice !=2 && choice!=0 && choice!=3){
 			system("clear");
-			messageError(1);
 		}
-	}while(choice!=1 && choice!=2 && choice!=0 && choice!=4);
+	}while(choice!=1 && choice!=2 && choice!=0 && choice!=3);
 	clearBuffer();
 	switch(choice){
 		case(1):
-			system("clear");  
+			printf("Enter your word : \n");
+			scanf("%s" , w); // enter a word ......
+			if(isAWord(w)){
+				byOcurrenceWord(w);
+				system("clear");
+			}
+			else printf("ERROR Word\n");
 			break;
-		case(2):  
-			system("clear");
+		case(2):
+			printf("Enter your color : \n");
+			scanf("%s" , w); // enter a word ......
+			if(isAColor(w)){
+				byColor(w);
+				system("clear");
+			}
+			else printf("ERROR Color\n");
 			break;
 		case(0): 
 			system("clear");
 			mainMenu(0); 
 			break;
-		case(4): 
-			printf("Test command unix :");
-			scanf("%c" , &a);
-			strcat(comm,&a);
-			system(comm); 
-			// BIG PROBLEM !!!!!!!!!
-			//    /!\ :/
-			ResearchMenu();
+		case(3):
+			printf("Enter your frequence : \n");
+			scanf("%f" , &freq); // enter a frequence ......
+			if(isAFreq){
+				bySOUNDSOUNDSOUND(freq);
+				system("clear");
+			}
+			else printf("ERROR Sound\n");
 			break;
 		default: 
-			messageError(0) ; 
 			exit(0); 
 			break;
 	}

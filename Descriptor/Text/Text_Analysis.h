03a546daccb5c79e84fa42693a37cfbe3fdf960d
sylvaran <estelle.randria@live.fr>
2014-12-30 17:00:37 +0100

    Update Text_Analysis.h

--- Descriptor/Text/Text_Analysis.h
+++ Descriptor/Text/Text_Analysis.h
@@ -34,3 +34,31 @@ Bool isWordRelevant (const char * word) ;
 
 //This function counts the number of appearances of a word in a text. 
 int wordAppearance (char * word) ;
+
+//This function counts the total of characters in the text. 
+int textNbChar(const char * path) ; 
+
+//This function checks if the term already exists in the index. 
+Bool doesTermExist (Index * i , Term term) ;
+
+//Initialisation of the index. 
+void initIndex (Index * i) ;
+
+//Check if the index is empty. 
+Bool indexEmpty (Index i) ; 
+
+//Initialisation of a term. 
+void initTerm (Term term) ;
+
+//Add a term in the index. 
+void addTerm (Index * i , Term term) ;
+
+//Remove a term from the index. 
+void removeFromIndex (Index * i , Term termtoremove) ;
+
+//Remove a term from the index under the condition that the number of appearances is inferior to the minimum required. 
+void removeTerm (Index * i) ; 
+
+
+//This function increments the appearances if the word already exist in the Index ; 
+void addOccurences (Index * i , Term t) ;

f1946d991117b7a56b7b58b0f0288f9bf935b80a
gabond <gab-dr@hotmail.fr>
2014-12-30 21:02:31 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+/**
+ * \brief The only one
+ * At the end of the project, the compilation should be OK :)
+*/
+
+
+void main() {
+  mainMenu(); // quite simply
+}
+
+/**
+ * \author M. CHABAUD, G. DEL REY ,E. RENDRIA,M.SANMARTIN
+ */
 

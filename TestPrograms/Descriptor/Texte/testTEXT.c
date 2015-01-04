d3f94e736ec00e1949ff358607efc688daa90d1d
sylvaran <estelle.randria@live.fr>
2015-01-04 13:06:48 +0100

    Update testTEXT.c
    All commentaries saying "checked" meant that the function's test was a success ! Way to go !!!

--- TestPrograms/Descriptor/Texte/testTEXT.c
+++ TestPrograms/Descriptor/Texte/testTEXT.c
@@ -9,7 +9,7 @@ int main () {
 	char w1[10] ; 
 	Term term1 , term2 ; 
 	strcpy (w, "sachet") ;
-	strcpy (w1, "chien") ;  
+	strcpy (w1, "calamar") ;  
 	
 	printf ("number of characters of the text : %d\n" , textNbchar("/home/mahenina/FIL_ROUGE/Test")) ;
 	initIndex(&I) ; 
@@ -21,8 +21,28 @@ int main () {
 	termDetails(term1) ; 
 	printf ("term1 creation :\n") ; createTerm(&term1, w) ; 
 	printf ("After the update : \n") ; 
-//createTerm() checked
+//createTerm() checked & termDetails() checked 
 	termDetails(term1) ;
 	increaseOccur(&term1) ; 
-	printf ("term1's occur : %d" , term1.occur) ; //affiche 1 au lieu d'afficher 2
+//increaseOccur() checked
+	termDetails(term1) ; 
+	printf ("term1's occur : %d\n" , term1.occur) ; 
+	decreaseOccur(&term1) ;
+//decreaseOccur() checked  
+	addTerm(&I, term1) ;
+	returnIndex(I) ; 
+//addTerm() and returnIndex() checked
+	createTerm(&term2 , w1) ; 
+	termDetails(term2) ; 
+	addTerm(&I , term2) ; 
+	returnIndex (I) ; 
+
+//Currently testing :
+	if (doesTermExist(&I, term1))
+		printf ("term1 exists\n") ; //it exists
+	else printf ("term1 doesn't exist\n") ;  
+	if (doesTermExist(&I , term2)) 
+		printf ("term2 exists\n") ;
+	else printf ("term2 doesn't exist\n") ; //it doesn't exist
+		
 } 

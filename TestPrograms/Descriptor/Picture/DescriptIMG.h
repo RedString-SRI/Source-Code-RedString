93b0973c3e05dda34af28a2a38d445d06cfabe2e
gabond <gab-dr@hotmail.fr>
2015-01-10 14:15:42 +0100

    Create DescriptIMG.h

---
+++ TestPrograms/Descriptor/Picture/DescriptIMG.h
@@ -0,0 +1,45 @@
+/**
+ * \file PictureDesc.h
+ * \brief picture desc
+ * \author Gabriel.DR
+ * \version 0.1
+ * \date 10 Janury 2015
+ *
+ */
+
+#include "Type_PictureDescritor.h"
+
+#ifndef PICTURE_Desc
+#define PICTURE_Desc
+
+/*Initialize a new picture's desc*/
+void initPictureDesc(PictureDesc *pd);
+
+/**
+ *\brief Give the size of the picture 
+ */
+Dimension getSizePicture(FILE *fileIMG);
+
+/**
+ *\brief create the picture's desc
+ */
+PictureDesc createPictureDesc(FILE *fileIMG);
+
+/**/
+void setNbcomp(int n);
+
+/*return nbcomp*/
+int getNbcomp();
+
+/**
+ *\brief Creat the image's histogramm
+*/
+void createHistogram(int bit[] , Dimension dim);
+
+/**
+ *\brief Print the image's histogramm
+*/
+void printHistogram(FILE *descriptIMG);
+
+
+#endif

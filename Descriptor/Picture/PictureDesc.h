17ea4d7ef159b045251317101acc735fb9963346
gabond <gab-dr@hotmail.fr>
2015-01-11 18:38:27 +0100

    Update PictureDesc.h

--- Descriptor/Picture/PictureDesc.h
+++ Descriptor/Picture/PictureDesc.h
@@ -41,5 +41,32 @@ void createHistogram(int bit[] , Dimension dim);
 */
 void printHistogram(FILE *descriptIMG);
 
+/**
+ * \brief Print a img desc in console
+ * 
+ * \param desc The desc to be displayed
+ */
+void printIMGDesc(IMGDesc desc);
 
+/**
+ * \brief Write a IMG desc to a file
+ * 
+ * Write desc to file (at the current position), so file
+ * needs to be opened in the approriate writing mode
+ * \param file The file you want to be written
+ * \param desc The desc which is going to be
+ * appended to the file
+ */
+void writeIMGDesc(FILE* file, IMGDesc* desc);
+
+/**
+ * \brief Read a sound desc from a file
+ * 
+ * Read a sound desc from a file (at the current position)
+ * and returns it, so the file needs to be opened in
+ * the approriate reading mode
+ * \param file The file you want to be read
+ * \return The desc which is going to store the read one
+ */
+IMGDesc readIMGDesc(FILE* file);
 #endif

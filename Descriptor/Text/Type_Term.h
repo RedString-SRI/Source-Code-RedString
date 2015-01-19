77827f54c5d5049f2d312bd6a38937245ab7bf9b
sylvaran <estelle.randria@live.fr>
2015-01-19 21:07:18 +0100

    Update Type_Term.h

--- Descriptor/Text/Type_Term.h
+++ Descriptor/Text/Type_Term.h
@@ -1,4 +1,7 @@
-
+//file Type_Term.h
+// brief All the functions necessary to analyse a text and then proceed to its indexation
+//Author Estelle Randria
+//Date : 11/28/2014
 
 #ifndef TYPE_TERM
 #define TYPE_TERM
